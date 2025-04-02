421bb26f96c8e1f26f932482ce1c5e4d2bd60de6
Sri-SriPod <srisripod123@gmail.com>
2025-04-02 14:39:01 +0800

    add lb macro

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,33 @@
 #include "main.h"
-#include "lemlib/api.hpp"
+#include "lemlib/api.hpp" // IWYU pragma: keep
 #include "lemlib/chassis/trackingWheel.hpp"
-
+#include "pros/adi.hpp"
+#include "pros/colors.hpp"
+#include "pros/distance.hpp"
+#include "pros/misc.h"
+#include "pros/rotation.hpp"
+#include "pros/rtos.hpp"
+#include "pros/screen.hpp"
+#include "pros/vision.hpp"
+#include "pros/llemu.hpp"
+#include "pros/adi.h"
 
 // controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // drivetrain motors
-pros::MotorGroup left_motor_group({-9, -19, -3}, pros::MotorGearset::blue); // left motors on ports 1, 2, 3
-pros::MotorGroup right_motor_group({20, 1, 2}, pros::MotorGearset::blue); // right motors on ports 19, 20, 18
+pros::MotorGroup left_motor_group({-1, -2, -3}, pros::MotorGearset::blue); // left motors on ports 1, 2, 3
+pros::MotorGroup right_motor_group({4, 5, 6}, pros::MotorGearset::blue); // right motors on ports 19, 20, 18
 pros::adi::DigitalOut mogo ('A');
-pros::adi::DigitalOut outtake ('F');
-pros::adi::DigitalOut doinker ('H');
-pros::Motor intake1 (4);
-pros::Motor intake2 (8);
+// pros::adi::DigitalOut doinker ('B');
+// pros::adi::DigitalOut mogorush('C');
+// pros::adi::DigitalOut ringrush('D');
+
+pros::Motor intake (20);
+pros::Motor lb(11);
+pros::Rotation lbrot(12);
+pros::Distance lbdist(19);
+pros::Optical lbcolor(14);
 
 bool mogo_value = false;
 bool doinker_value = false;
@@ -21,49 +35,51 @@ bool doinker_value = false;
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
                               &right_motor_group, // right motor group
-                              12.437, // track width
+                              9.829, // track width
                               lemlib::Omniwheel::NEW_275, 
-                              480, 
+                              480,
                               2
 );
 
-// create an imu on port 5
-pros::Imu imu(15);
+pros::Imu imu(8);
+pros::Rotation vertical_encoder(9);
+pros::Rotation horizontal_encoder(10);
 
-// pros::Rotation vertical_sensor(15);
-pros::Rotation horizontal_sensor(7);
-lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_sensor, lemlib::Omniwheel::NEW_2, 0);
-// lemlib::TrackingWheel vertical_tracking_wheel(&vertical_sensor, lemlib::Omniwheel::NEW_2, -2.5);
+// offset of the tracking wheel is equal to the length of the line perpendicular to it that ends at the tracking center
 
-lemlib::OdomSensors sensors(NULL, // vertical tracking wheel 1, set to null
-                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, lemlib::Omniwheel::NEW_2, 0);
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, lemlib::Omniwheel::NEW_2, 0);
+
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we don't have a second one
                             &horizontal_tracking_wheel, // horizontal tracking wheel 1
                             nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
                             &imu // inertial sensor
 );
 // lateral PID controller
-lemlib::ControllerSettings lateral_controller(6.7, // proportional gain (kP)
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              1, // derivative gain (kD)
-                                              0.45, // anti windup
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
                                               1, // small error range, in inches
                                               100, // small error range timeout, in milliseconds
                                               3, // large error range, in inches
                                               500, // large error range timeout, in milliseconds
-                                              0 // maximum acceleration (slew) (tune this)
+                                              20 // maximum acceleration (slew)
 );
 
 // angular PID controller
-lemlib::ControllerSettings angular_controller(3, // proportional gain (kP)
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              17.2, // derivative gain (kD)
-                                              0.45, // anti windup
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
                                               1, // small error range, in degrees
                                               100, // small error range timeout, in milliseconds
                                               3, // large error range, in degrees
                                               500, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
+
 // input curve for throttle input during driver control
 lemlib::ExpoDriveCurve throttle_curve(3, // joystick deadband out of 127
                                      10, // minimum output where drivetrain will move out of 127
@@ -84,46 +100,65 @@ lemlib::Chassis chassis(drivetrain,
                         &throttle_curve, 
                         &steer_curve
 );
-void intake(){
-    intake1.move(-115);
-    intake2.move(115);
+
+
+
+const int numStates = 3;
+int states[numStates] = {5, 34, 150};
+int currState = 0;
+int target = states[0];
+
+void nextState() {
+    currState += 1;
+    if (currState == numStates) {
+        currState = 0;
+    }
+    target = states[currState];
 }
+
+void ladybrown() {
+    double kp = 2;
+    double error = target - (lbrot.get_position()/100.0);
+    double velocity = kp * error;
+    lb.move(velocity);
+}
+
 
 void initialize() {
 	// print encoder readings to the brain
 	pros::lcd::initialize(); // initialize brain screen
-    auto imus = pros::Imu::get_all_devices();
-    for (auto device : imus) {
-        device.reset();
-    }
-    for (auto device : imus) {
-        while (device.is_calibrating())
-        pros::delay(100);
-    }
-    // chassis.calibrate(); // calibrate sensors
-    chassis.calibrate(true); // calibrate sensors
-    mogo.set_value(false);
+    chassis.calibrate();
+    // auto imus = pros::Imu::get_all_devices();
+    // for (auto device : imus) {
+    //     device.reset();
+    // }
+    // for (auto device : imus) {
+    //     while (device.is_calibrating())
+    //     pros::delay(100);
+    // }
+    // chassis.calibrate(true); // calibrate sensors
     pros::Task screen_task([&]() {
         while (true) {
-            
-			// print measurements from the rotation sensor
-			// pros::lcd::print(0, "Vertical: %i", vertical_sensor.get_position());
-			pros::delay(10); // delay to save resources. DO NOT REMOVE
+            pros::lcd::print(0, "Lb position: %d", (lbrot.get_position()/100));
+            // pros::lcd::print(0, "Lb distance %f!", lbdist.get_distance());
+            // pros::lcd::print(0, "Lb confidence %f!", lbdist.get_confidence());
+            pros::delay(10);
+        }
+    });
+    pros::Task lbtask([&]() {
+        while (true) {
+            pros::lcd::print(1, "target: %d", target);
+            pros::lcd::print(2, "distance: %d", lbdist.get_distance());
+            pros::lcd::print(3, "distance: %d", lbdist.get_confidence());
 
-			/**
-			When you push the robot forwards, the measured position of the vertical encoder(s) should increase. 
-			If they decrease, the sensor(s) needs to be reversed. 
-			When you push the robot to the right (relative to the robot), the position measured by horizontal encoders should increase. 
-			If they decrease, the sensor(s) needs to be reversed.
-			*/
-            
-			// print robot location to the brain screen
-            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
-            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
-            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
-            // pros::lcd::print(3, "Horizontal: %i", horizontal_sensor.get_position());
-            // delay to save resources
-            pros::delay(20);
+            ladybrown();
+            // if (lbdist.get_distance() < 10 && (lbdist.get_distance() > 5)){
+            //     intake.brake();
+            //     pros::lcd::print(4, "bababooey: %d", lbdist.get_distance());
+            // }
+            // pros::lcd::print(0, "Lb distance %f!", lbdist.get_distance());
+            // pros::lcd::print(0, "Lb confidence %f!", lbdist.get_confidence());
+            pros::delay(10);
         }
     });
 }
@@ -144,6 +179,7 @@ void disabled() {}
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
+
 void competition_initialize() {}
 
 
@@ -158,14 +194,8 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void rerun(){
-    pros::delay(500);
-}
 
-void autonomous() {
-
-    }
-
+void autonomous() {}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -183,13 +213,14 @@ void autonomous() {
 void opcontrol() {
     while (true) {
         // move the robot
+
         int rightY = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
         int leftX = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
         chassis.arcade(rightY, leftX);
+
+        // if want to prioritize turning over throttle, value closer to 1.
+        // chassis.arcade(rightY, leftX, false, 0.75);
         
-
-        // get left y and right x positions
-
         if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){
             if (mogo_value){
                 mogo.set_value(false);
@@ -203,39 +234,31 @@ void opcontrol() {
             pros::delay(170); // how long you can press it for
 
         }
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)){
-            if (doinker_value){
-                doinker.set_value(false);
-                doinker_value = false;
-            }
-            else{
-                doinker.set_value(true);
-                doinker_value = true;
-            }
+        // if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)){
+        //     if (doinker_value){
+        //         doinker.set_value(false);
+        //         doinker_value = false;
+        //     }
+        //     else{
+        //         doinker.set_value(true);
+        //         doinker_value = true;
+        //     }
 
-            pros::delay(170); // how long you can press it for
-        }
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
-            intake();
-        }
-        // else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)){
-        //     intake1.move(-127);
-        //     intake2.move(25);
+        //     pros::delay(170); // how long you can press it for
         // }
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
+            intake.move(127);
+        }
         else{
-            intake1.brake();
-            intake2.brake();
+            intake.brake();
         }
         
-        
-        pros::delay(25); 
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
-            rerun();
-
-            pros::delay(250); // how long you can press it for
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)){
+            nextState();
+            pros::delay(170);
         }
         
-        
+        pros::delay(20);
     }
 
 }
